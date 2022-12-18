4919edb2df50ca1c0eee65ea50b6b59759b29f13
dimi7rof <84029988+dimi7rof@users.noreply.github.com>
2022-12-18 13:23:03 +0200

    Update Day2.cs

--- Day2.cs
+++ Day2.cs
@@ -25,3 +25,25 @@ foreach (var item in input)
 Console.WriteLine(sum);
 
 //Part2
+
+var results = new Dictionary<string, int>()
+{
+    { "A X", 3},
+    { "A Y", 4},
+    { "A Z", 8},
+    { "B X", 1},
+    { "B Y", 5},
+    { "B Z", 9},
+    { "C X", 2},
+    { "C Y", 6},
+    { "C Z", 7}
+};
+string[] input = Inputs.day2input.Split(Environment.NewLine);
+int sum = 0;
+
+foreach (var item in input)
+{
+    sum += results[item];
+}
+
+Console.WriteLine(sum);
